2f0434921f814ae5443497f73516d7814a2095b0
Huda008 <36947701+Huda008@users.noreply.github.com>
2018-03-08 15:39:49 +1100

    Update ASD1709HardeningComplianceCheck.ps1
    Computer Identity Authentication for NTLM is used

--- ASD1709HardeningComplianceCheck.ps1
+++ ASD1709HardeningComplianceCheck.ps1
@@ -1562,17 +1562,20 @@ write-host " Network access: Let Everyone permissions apply to anonymous users i
 $RestrictNullSessAccess = Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\LanManServer\Parameters\ -Name RestrictNullSessAccess -ErrorAction SilentlyContinue|Select-Object -ExpandProperty RestrictNullSessAccess
 if ( $RestrictNullSessAccess -eq $null)
 {
-write-host " Network access: Do not allow anonymous enumeration of SAM accounts and shares is not configured" -ForegroundColor Yellow
+write-host " Network access: Restrict anonymous access to Named Pipes and Shares is not configured" -ForegroundColor Yellow
 }
    elseif ( $RestrictNullSessAccess  -eq  '1' )
 {
-write-host " Network access: Restrict clients allowed to make remote calls to SAM is configured correctly" -ForegroundColor Green
+write-host " Network access: Restrict anonymous access to Named Pipes and Shares is enabled" -ForegroundColor Green
+}
+ elseif ( $RestrictNullSessAccess  -eq  '0' )
+{
+write-host " Network access: Restrict anonymous access to Named Pipes and Shares is disabled" -ForegroundColor Red
 }
    else
 {
-write-host " Network access: Restrict clients allowed to make remote calls to SAM is configured incorrectly" -ForegroundColor Red
+write-host " Network access: Restrict anonymous access to Named Pipes and Shares is set to an unknown setting " -ForegroundColor Red
 }
-
 
 $RestrictRemoteSAM = Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa\ -Name RestrictRemoteSAM -ErrorAction SilentlyContinue|Select-Object -ExpandProperty RestrictRemoteSAM
 if ( $RestrictRemoteSAM -eq $null)
@@ -1581,38 +1584,30 @@ write-host " Network access: Do not allow anonymous enumeration of SAM accounts 
 }
    elseif ( $RestrictRemoteSAM  -eq  'O:BAG:BAD:(A;;RC;;;BA)' )
 {
-write-host " Network access: Do not allow anonymous enumeration of SAM accounts and shares is enabled" -ForegroundColor Green
+write-host " Network access: Do not allow anonymous enumeration of SAM accounts and shares is configured correctly" -ForegroundColor Green
 }
-  elseif ( $RestrictRemoteSAM  -eq  '0' )
+    else
 {
-write-host " Network access: Do not allow anonymous enumeration of SAM accounts and shares is disabled" -ForegroundColor Red
+write-host " Network access: Do not allow anonymous enumeration of SAM accounts and shares isconfigured incorrectly." -ForegroundColor Red
 }
-  else
-{
-write-host " Network access: Do not allow anonymous enumeration of SAM accounts and shares is set to an unknown setting" -ForegroundColor Red
-}
-
-
-write-host "Network security: Allow Local System to use computer identity for NTLM"
-
 
 
 $UseMachineId = Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Lsa\ -Name UseMachineId -ErrorAction SilentlyContinue|Select-Object -ExpandProperty UseMachineId
 if ( $UseMachineId -eq $null)
 {
-write-host "Network security: Allow Local System to use computer identity for NTLM is not configured" -ForegroundColor Yellow
+write-host " Network security: Allow Local System to use computer identity for NTLM is not configured" -ForegroundColor Yellow
 }
    elseif ( $UseMachineId  -eq  '1' )
 {
-write-host "Network security: Allow Local System to use computer identity for NTLM is enabled" -ForegroundColor Green
+write-host " Network security: Allow Local System to use computer identity for NTLM is enabled" -ForegroundColor Green
 }
-  elseif ( $UseMachineId  -eq  '0' )
+  elseif ( $EUseMachineId  -eq  '1' )
 {
-write-host "Network security: Allow Local System to use computer identity for NTLM is disabled" -ForegroundColor Red
+write-host " Network security: Allow Local System to use computer identity for NTLM is disabled" -ForegroundColor Red
 }
   else
 {
-write-host "Network security: Allow Local System to use computer identity for NTLM is set to an unknown setting" -ForegroundColor Red
+write-host " Network security: Allow Local System to use computer identity for NTLM is set to an unknown setting" -ForegroundColor Red
 }
 
 
@@ -2130,91 +2125,10 @@ write-host " Specify the maximum log file size (KB) for the Setup Log is set to 
 write-host " Specify the maximum log file size (KB) for the Setup Log is set to an unknown setting" -ForegroundColor Red
 }
 
-write-host "`r`nManage Auditing and Security Log is unable to be checked using PowerShell, as the setting is not a registry key. Please check Computer Configuration\Policies\Windows Settings\Security Settings\Local Policies\User Rights Assignment. ASD Recommendation is to only have 'Administrators' present" -ForegroundColor Cyan
+write-host "Manage Auditing and Security Log is unable to be checked using PowerShell, as the setting is not a registry key. Please check Computer Configuration\Policies\Windows Settings\Security Settings\Local Policies\User Rights Assignment. ASD Recommendation is to only have 'Administrators' present" -ForegroundColor Cyan
 
-write-host "`r`nAudit Credential Validation is unable to be checked using PowerShell, as the setting is not a registry key. Please check Computer Configuration\Policies\Windows Settings\Security Settings\Advanced Audit Policy Configuration\Audit Policies\Account Logon. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
 
-write-host "`r`nFor the below controls please check: Computer Configuration\Policies\Windows Settings\Security Settings\Advanced Audit Policy Configuration\Audit Policies\Account Management"  -ForegroundColor Cyan
-write-host "    Audit Computer Account Management is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit Other Account Management Events is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit Security Group Management is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit User Account Management is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "`r`nFor the below controls please check: Computer Configuration\Policies\Windows Settings\Security Settings\Advanced Audit Policy Configuration\Audit Policies\Detailed Tracking"  -ForegroundColor Cyan
-
-write-host "    Audit PNP Activity is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success' Present" -ForegroundColor Cyan
-
-write-host "    Audit Process Creation is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success' Present" -ForegroundColor Cyan
-
-write-host "    Audit Process Termination is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success' Present" -ForegroundColor Cyan
-
-write-host "`r`nFor the below controls please check: Computer Configuration\Policies\Windows Settings\Security Settings\Advanced Audit Policy Configuration\Audit Policies\Logon/Logoff"  -ForegroundColor Cyan
-
-write-host "    Audit Account Lockout is unable to be checked using PowerShell, as the setting is not a registry key. Please check. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit Group Membership is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success' Present" -ForegroundColor Cyan
-
-write-host "    Audit Logoff is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success' Present" -ForegroundColor Cyan
-
-write-host "    Audit Logon is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit Other Logon/Logoff Events is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit Audit Special Logon is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "`r`nFor the below controls please check: Computer Configuration\Policies\Windows Settings\Security Settings\Advanced Audit Policy Configuration\Audit Policies\Object Access"  -ForegroundColor Cyan
-
-write-host "    Audit File Share is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit Kernel Object is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit Other Object Access Events is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit Removable Storage is unable to be checked using PowerShell, as the setting is not a registry key ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "`r`nFor the below controls please check: Computer Configuration\Policies\Windows Settings\Security Settings\Advanced Audit Policy Configuration\Audit Policies\Policy Change"  -ForegroundColor Cyan
-
-write-host "    Audit Audit Policy Change is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit Authentication Policy Change is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success' Present" -ForegroundColor Cyan
-
-write-host "    Audit Authorization Policy Change is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success' Present" -ForegroundColor Cyan
-
-write-host "`r`nAudit Sensitive Privilege Use is unable to be checked using PowerShell, as the setting is not a registry key. Please check Computer Configuration\Policies\Windows Settings\Security Settings\Advanced Audit Policy Configuration\Audit Policies\Privilege Use. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "`r`nFor the below controls please check: Computer Configuration\Policies\Windows Settings\Security Settings\Advanced Audit Policy Configuration\Audit Policies\System"  -ForegroundColor Cyan
-
-write-host "    Audit IPsec Driver is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit Other System Events is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit Security State Change is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success' Present" -ForegroundColor Cyan
-
-write-host "    Audit Security System Extension is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-write-host "    Audit System Integrity is unable to be checked using PowerShell, as the setting is not a registry key. ASD Recommendation is to have 'Success and Failure' Present" -ForegroundColor Cyan
-
-$SCENoApplyLegacyAuditPolicy = Get-ItemProperty -Path  'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Lsa\'  -Name SCENoApplyLegacyAuditPolicy -ErrorAction SilentlyContinue|Select-Object -ExpandProperty SCENoApplyLegacyAuditPolicy
-if ( $SCENoApplyLegacyAuditPolicy -eq $null)
-{
-write-host "Audit: Force audit policy subcategory settings (Windows Vista or later) to override audit policy category settings is not configured" -ForegroundColor Yellow
-}
-   elseif ( $SCENoApplyLegacyAuditPolicy -eq  '1' )
-{
-write-host "Audit: Force audit policy subcategory settings (Windows Vista or later) to override audit policy category settings is enabled" -ForegroundColor Green
-}
-  elseif ( $SCENoApplyLegacyAuditPolicy  -eq  '0' )
-{
-write-host "Audit: Force audit policy subcategory settings (Windows Vista or later) to override audit policy category settings is disabled" -ForegroundColor Red
-}
-  else
-{
-write-host "Audit: Force audit policy subcategory settings (Windows Vista or later) to override audit policy category settings is set to an unknown setting" -ForegroundColor Red
-}
-
+write-host "Check whole Audit Event Management Section, unable to do a lot of them"
 
 write-host "`r`n####################### AUTOPLAY AND AUTORUN #######################`r`n"
 
